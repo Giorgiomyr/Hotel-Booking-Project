947fd919ead1434b73cc04eeb2560c14ab77af74
giorgiomyr <giorgiomyr@hotmail.com>
2019-04-15 15:41:23 +0300

    ok

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("up");
+jQuery( init );
+
+// same as jQuery(document).ready( init );
+
+//Alternative no global vars at all
+//jQuery(function init()(...));
+
+// Our apliication logic goes here
+
+function init( $ ){
+    console.log( "Ready" );
+}
